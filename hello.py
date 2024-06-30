da0602ae116e8dfe60311dfb3152c5e6acd0b970
Dennis <162328452+EngDePal@users.noreply.github.com>
2024-06-30 21:10:31 +0200

    Testing Flask functionality

---
+++ hello.py
@@ -0,0 +1,8 @@
+"""Testing proper installation of Flask"""
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return "Hello, World!"
--- packages/server.py
+++ packages/server.py
@@ -1,1 +1,17 @@
 """Handles the logic of the core application, especially HTTP requests and access to different manager classes"""
+#Importing necessary modules and classes from the Flask web framework
+from flask import Flask, request, jsonify
+
+#Create a flask app
+app = Flask(__name__)
+
+#The following sections defines the handling of incoming http requests
+#For more details on the individual commands please check GitHub or the accompanying thesis
+
+
+@app.route('/<test>')
+def home(test):
+    return f"Hello, {test}!"
+
+while True:
+    app.run()
